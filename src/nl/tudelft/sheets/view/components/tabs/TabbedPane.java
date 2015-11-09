4f92ec1d9232fcaa0afe8cbea3b042f6a7f329d5
Jasper Ketelaar <jasperketelaar@kpnmail.nl>
2015-11-09 02:43:53 +0100

    Some javadoc commenting

--- src/nl/tudelft/sheets/view/components/tabs/TabbedPane.java
+++ src/nl/tudelft/sheets/view/components/tabs/TabbedPane.java
@@ -4,18 +4,31 @@ package nl.tudelft.sheets.view.components.tabs;
 import nl.tudelft.sheets.view.Application;
 
 import javax.swing.*;
-
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 /**
+ * <p>
+ * A class to create the tabs; has no unique functionality but is created anyways for future functionality yet
+ * to be implemented.
+ * </p>
+ * <p>
  * Created by Jasper on 11/3/2015.
  */
 public class TabbedPane extends JTabbedPane {
 
+
+    /**
+     * The parent of this JTabbedPane (which is an Application instance), currently not used but is here for future
+     * usage
+     */
     private final Application parent;
 
+    /**
+     * TabbedPane constructor that takes an Application instance as parameter. Also sets the position of the tabs at
+     * the bottom of the application and sets the initial size of the application.
+     *
+     * @param parent the parent of this JTabbedPane which is an Application instance
+     */
     public TabbedPane(final Application parent) {
         super(JTabbedPane.BOTTOM);
         setPreferredSize(new Dimension(500, 400));
@@ -23,18 +36,16 @@ public class TabbedPane extends JTabbedPane {
 
     }
 
-    public Application getApplication() {
-        return parent;
-    }
-
+    /**
+     * Overrides addTab from JTabbedPane to make it select the newly added component.
+     *
+     * @param name      the name of the new tab
+     * @param component the component which this new tab should show
+     */
     @Override
     public void addTab(final String name, final Component component) {
         super.addTab(name, component);
         this.setSelectedComponent(component);
-
-    }
-
-    public class RightClick extends MouseAdapter {
 
     }
 }
--- src/nl/tudelft/sheets/view/listener/ResizeListener.java
+++ src/nl/tudelft/sheets/view/listener/ResizeListener.java
@@ -6,27 +6,51 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 /**
+ * <p>
+ * Listens a resize event of the main application. If this event occurs all of the components of the JTabbedPane
+ * that the application has set as content panel will be resized accordingly. Extends ComponentAdapter instead of
+ * implementing ComponentListener for simplicity and compactness.
+ * </p>
+ * <p>
  * Created by jasperketelaar on 11/1/15.
  */
 public class ResizeListener extends ComponentAdapter {
 
+    /**
+     * The JTabbedPane which contains these components
+     */
     private final JTabbedPane panel;
 
 
+    /**
+     * ResizeListener constructor, takes a JFrame as parameter gets the JTabbedPane by assuming it is the content panel
+     * of the frame.
+     *
+     * @param component the JFrame which is supposed to have a JTabbedPane as content panel
+     */
     public ResizeListener(final JFrame component) {
         this.panel = (JTabbedPane) component.getContentPane();
     }
 
+    /**
+     * <p>
+     * Gets called when the application (the component this even is bound to) gets resized, will go through every
+     * component the application has. If this component is an instance of a JScrollPane (Which are the only components
+     * that need to be resized), this component is casted to a JScrollPane and the size of this scroll pane is then
+     * set to the current sizeof the main component.
+     * </p>
+     *
+     * @param evt the event containing information such as the source of the event which we need to get the size of
+     *            the resized component.
+     */
     @Override
     public void componentResized(final ComponentEvent evt) {
-
         for (final Component comp : panel.getComponents()) {
-            if (!(comp instanceof JScrollPane)) continue;
-            final JScrollPane scroll = (JScrollPane) comp;
-            final Dimension size = evt.getComponent().getSize();
-            scroll.setPreferredSize(size);
-            scroll.getParent().setPreferredSize(new Dimension(size.width + 50, size.height));
-
+            if (comp instanceof JScrollPane) {
+                final JScrollPane scroll = (JScrollPane) comp;
+                final Dimension size = evt.getComponent().getSize();
+                scroll.setPreferredSize(size);
+            }
         }
     }
 }
